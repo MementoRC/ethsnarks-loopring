e86c933548f4d6ced8b3ba166f4908bb370e11d7
Michael Borraccia <michael@tokamak.ca>
2020-10-27 16:06:10 -0400

    Remove these imports

--- __init__.py
+++ __init__.py
@@ -1,3 +1,1 @@
-from .ethsnarks_loopring.eddsa import PureEdDSA, PoseidonEdDSA
-from .ethsnarks_loopring.field import FQ, SNARK_SCALAR_FIELD
-from .ethsnarks_loopring.poseidon import poseidon_params, poseidon
+
