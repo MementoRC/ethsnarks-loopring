db440156e1fc25adb619b972b0e5731d226ab320
Michael Borraccia <michael@tokamak.ca>
2020-10-27 14:11:24 -0400

    Fix imports

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .ethsnarks.eddsa import PureEdDSA, PoseidonEdDSA
-from .ethsnarks.field import FQ, SNARK_SCALAR_FIELD
-from .ethsnarks.poseidon import poseidon_params, poseidon
+from .ethsnarks_loopring.eddsa import PureEdDSA, PoseidonEdDSA
+from .ethsnarks_loopring.field import FQ, SNARK_SCALAR_FIELD
+from .ethsnarks_loopring.poseidon import poseidon_params, poseidon
